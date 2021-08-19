6231c2d5fa85c0e8aa6dcfb18e8805b468f1510e
noda <kohei-noda@hiroshima-u.ac.jp>
2021-08-19 19:53:36 +0900

    Probably succeeded in creating read_mdcint_debug.f90 that can read MDCINT

--- i4_read_mdcint_debug.f90
+++ i4_read_mdcint_debug.f90
@@ -1,45 +1,42 @@
 program tab_num_test
     implicit none
-    integer::i, mdcint = 11, debug = 12, ikr, jkr, nz, inz, nkr, i0
-    integer, allocatable :: indk(:), indl(:), kr(:)
+    integer::i, mdcint = 11, debug = 12,  inz
+    integer(4) :: ikr, jkr, nz
+    integer(4), allocatable :: indk(:), indl(:)
     double precision, allocatable :: rklr(:), rkli(:)
     character*50::Filename, mdcintBaseName, mdcint_debug, mdcintNum
     character*100:: errmsg
-    character  :: datex*10, timex*8
+
     integer :: nmo = 192
     inz = 1
     ! do i = 1, 8
-        Allocate(kr(-nmo/2:nmo/2))
         Allocate(indk(nmo**2))
         Allocate(indl(nmo**2))
         Allocate(rklr(nmo**2))
         Allocate(rkli(nmo**2))
         mdcintBaseName = "MDCINXXXX"
-        ! if ( i == 1 ) then
+        if ( i == 1 ) then
             Filename = "MDCINT"
             ! mdcintNew = "MDCINTNEW"
             mdcint_debug = "MDCINT_debug"
             ! mdcint_int = "MDCINT_int"
-        ! elsecd
-            ! write(mdcintNum,"(I3)") i-1
-            ! Filename = trim(mdcintBaseName)//trim(adjustl(mdcintNum))
+        else
+            write(mdcintNum,"(I3)") i-1
+            Filename = trim(mdcintBaseName)//trim(adjustl(mdcintNum))
             ! mdcintNew = "MDCINTNEW"
-            ! mdcint_debug = "MDCINT_debug"//trim(adjustl(mdcintNum))
+            mdcint_debug = "MDCINT_debug"//trim(adjustl(mdcintNum))
             ! mdcint_int = "MDCINT_int"
-        ! end if
+        end if
         open(mdcint, file=Filename, form="unformatted", status="unknown")
         open(debug, file=mdcint_debug, form="formatted", status="unknown")
         open(100, file="mdcintfiles_debug", form="formatted", status="unknown")
         write(100,*) Filename, mdcint_debug
         ! close(100)
-        ! read (mdcint)
-        read (mdcint) datex,timex,nkr, (kr(i0),kr(-1*i0),i0=1,nkr)
+        read (mdcint)
         ! read(mdcint, end=100, err=110, iomsg=errmsg) ikr,jkr, nz, (indk(inz),indl(inz), rklr(inz),rkli(inz), inz=1,nz)
         ! do inz = 1, nz
         do
-            read(mdcint,end=100, err=110, iomsg=errmsg) ikr,jkr, nz, &
-                    (indk(inz),indl(inz), inz=1,nz), &
-                    (rklr(inz), inz=1,nz)
+            read(mdcint,end=100, err=110, iomsg=errmsg) ikr,jkr, nz, (indk(inz),indl(inz), rklr(inz), inz=1,nz)
             do inz = 1, nz
                 write(debug, "(5I20,E32.16)") ikr, jkr, nz, indk(inz), indl(inz), rklr(inz)
             end do
