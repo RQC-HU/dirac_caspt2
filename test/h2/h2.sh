639c97ec0826c28f21ed01996789449b08478f7a
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-06-15 08:20:01 +0900

    Fix path

--- test/h2/h2.sh
+++ test/h2/h2.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
 # H2, 1angstrom, STO-3G
-abspath="$(dirname $0)"
-binarypath="$abspath/../../bin"
+abspath="$(cd $(dirname $0); pwd)" # Get the path of this script
+cd "$abspath" || return 1 # cd to the path of this script
+binarypath="../../bin" # Binary path is specified by relative path
+
+# Object files path
 r4dcasci="$binarypath/r4dcascicoexe"
 r4dcaspt2="$binarypath/r4dcaspt2ocoexe"
+
+# Output path
+outpath="$abspath/H2.caspt2.out"
+
 echo "h2.sh abspath $abspath $binarypath $r4dcasci $r4dcaspt2"
 # OMP_NUM_THREADS=4
 # Execute CASCI/CASPT2 programs
-$r4dcasci &> H2.caspt2.out
-$r4dcaspt2 &>> H2.caspt2.out
+$r4dcasci &> $outpath
+$r4dcaspt2 &>> $outpath
 
 # Remove unnecessary files
 rm [A-H]*int* MDCINTNEW NEWCICOEFF CIMAT* e0after EPS TRANSFOCK
--- test/h2/test_h2.py
+++ test/h2/test_h2.py
@@ -6,35 +6,30 @@ import pytest
 def test_h2():
     test_path = os.path.dirname(os.path.abspath(__file__))
     os.chdir(test_path)  # Change directory to dirac_caspt2/test/h2
-    r4dcascicoexe = os.path.join(test_path, "../../bin/r4dcascicoexe")
-    r4dcaspt2ocoexe = os.path.join(test_path, "../../bin/r4dcaspt2ocoexe")
-    print(test_path, r4dcascicoexe, r4dcaspt2ocoexe)
+    print(test_path)
+    # r4dcascicoexe = os.path.join(test_path, "../../bin/r4dcascicoexe")
+    # r4dcaspt2ocoexe = os.path.join(test_path, "../../bin/r4dcaspt2ocoexe")
+    # print(test_path, r4dcascicoexe, r4dcaspt2ocoexe)
     ref_filename = "reference.H2.out"
-    ref_filepath = os.path.join(test_path, ref_filename)
+    # ref_filepath = os.path.join(test_path, ref_filename)
     output_filename = "H2.caspt2.out"
-    output_filepath = os.path.join(test_path, output_filename)
+    # output_filepath = os.path.join(test_path, output_filename)
+    prev_output = "H2.caspt2.out.prev"
+    # prev_output_filepath = os.path.join(test_path, prev_output)
+    # print("output", ref_filename, ref_filepath, output_filename, output_filepath)
     # Run calculation
-    # p = subprocess.run("sh " + test_path + "/h2.sh", shell=True)
-    # CASCI
-    p = subprocess.run(r4dcascicoexe + " &> " + output_filename, shell=True)
-    print("CASCI status", p.returncode)
-    # CASPT2
-    p = subprocess.run(r4dcaspt2ocoexe + " &>> " + output_filename, shell=True)
-    print("CASPT2 status", p.returncode)
-    # Remove scratch files
-    p = subprocess.run(
-        "rm [A-H]*int* CIMAT* e0after EPS MDCINTNEW NEWCICOEFF TRANSFOCK", shell=True
-    )
+    p = subprocess.run("sh ./h2.sh", shell=True)
+    print("CASCI/CASPT2 status", p.returncode)
     p = subprocess.run("ls", shell=True)
     ref = subprocess.run(
-        "cat " + ref_filepath + " |  awk ' /Total/{print}' | tr -s '\n'",
+        "cat " + ref_filename + " |  awk ' /Total/{print}' | tr -s '\n'",
         shell=True,
         encoding="utf-8",
         stdout=subprocess.PIPE,
     )  # Get CASPT2 energy and e2sum from the output
     print("ref output awk end")
     output = subprocess.run(
-        "cat " + output_filepath + " | awk '/Total/{print}' | tr -s '\n'",
+        "cat " + output_filename + " | awk '/Total/{print}' | tr -s '\n'",
         shell=True,
         encoding="utf-8",
         stdout=subprocess.PIPE,
@@ -49,6 +44,8 @@ def test_h2():
     # Check whether the output of test run
     # matches the reference to 7th decimal places.
     assert output_energy == pytest.approx(ref_energy, 1e-8)
+    # The previous output file is overwritten by the current output file.
+    subprocess.run("mv " + output_filename + " " + prev_output, shell=True)
 
 
 if __name__ == "__main__":
