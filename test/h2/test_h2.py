67ba43738fc65dceb2d5d7110ff6424a53f9b84e
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-06-15 07:06:06 +0900

    Change not to run h2.sh

--- test/h2/test_h2.py
+++ test/h2/test_h2.py
@@ -5,10 +5,23 @@ import pytest
 
 def test_h2():
     test_path = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(test_path) # Change directory to
+    os.chdir(test_path)  # Change directory to dirac_caspt2/test/h2
+    r4dcascicoexe = os.path.join(test_path, "../../bin/r4dcascicoexe")
+    r4dcaspt2ocoexe = os.path.join(test_path, "../../bin/r4dcaspt2ocoexe")
+    print(test_path, r4dcascicoexe, r4dcaspt2ocoexe)
     output_filename = "H2.caspt2.out"
-    p = subprocess.run("sh " + test_path + "/h2.sh", shell=True)  # Run calculation
-    print(p.returncode)
+    # Run calculation
+    # p = subprocess.run("sh " + test_path + "/h2.sh", shell=True)
+    # CASCI
+    p = subprocess.run(r4dcascicoexe + " &> " + output_filename, shell=True)
+    print("CASCI status", p.returncode)
+    # CASPT2
+    p = subprocess.run(r4dcaspt2ocoexe + " &>> " + output_filename, shell=True)
+    print("CASPT2 status", p.returncode)
+    # Remove scratch files
+    p = subprocess.run(
+        "rm [A-H]*int* CIMAT* e0after EPS MDCINTNEW NEWCICOEFF TRANSFOCK", shell=True
+    )
     ref = subprocess.run(
         "cat "
         + test_path
@@ -17,7 +30,7 @@ def test_h2():
         shell=True,
         encoding="utf-8",
         stdout=subprocess.PIPE,
-    ) # Get CASPT2 energy and e2sum from the output
+    )  # Get CASPT2 energy and e2sum from the output
     output = subprocess.run(
         "cat "
         + test_path
@@ -27,11 +40,16 @@ def test_h2():
         shell=True,
         encoding="utf-8",
         stdout=subprocess.PIPE,
-    ) # Get CASPT2 energy and e2sum from the output
+    )  # Get CASPT2 energy and e2sum from the output
+
+    # Check output for debugging
+    print("ref.stdout", ref.stdout)
+    print("ref.stdout", output.stdout)
     # Convert string to float
     ref_energy = float(ref.stdout.split()[-2])
     output_energy = float(output.stdout.split()[-2])
-    # Check whether the output of test run matches the reference to 7th decimal places.
+    # Check whether the output of test run
+    # matches the reference to 7th decimal places.
     assert output_energy == pytest.approx(ref_energy, 1e-8)
 
 
