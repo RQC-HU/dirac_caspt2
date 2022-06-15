6ada4ca98bcf492444784d0ebca5aa60f68ed6b0
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-06-16 00:59:42 +0900

    Fix file path

--- test/h2/test_h2.py
+++ test/h2/test_h2.py
@@ -9,9 +9,9 @@ def test_h2():
     os.chdir(test_path)  # Change directory to dirac_caspt2/test/h2
     print(test_path)  # Debug output
     ref_filename = "reference.H2.out"  # Reference
-    ref_file_path = os.path.normpath(os.path.join(test_path, "/", ref_filename))
+    ref_file_path = os.path.normpath(os.path.join(test_path, ref_filename))
     output_filename = "H2.caspt2.out"  # Output (This file is compared with Reference)
-    output_file_path = os.path.normpath(os.path.join(test_path, "/", output_filename))
+    output_file_path = os.path.normpath(os.path.join(test_path, output_filename))
     r4dcasci = os.path.normpath(os.path.join(test_path, "../../bin/r4dcascicoexe"))
     r4dcaspt2 = os.path.normpath(os.path.join(test_path, "../../bin/r4dcaspt2ocoexe"))
     # Run calculation
@@ -36,7 +36,7 @@ def test_h2():
 
     # Delete scratch files
     for d in delete_files:
-        files = glob.glob("".join([test_path, "/", d]))
+        files = glob.glob(os.path.normpath(os.path.join(test_path, d)))
         for f in files:
             os.remove(f)
     # Check output
@@ -44,9 +44,8 @@ def test_h2():
         print(f.read())
     # Grep the reference output file
     with open(ref_file_path, encoding="utf-8", mode="r") as f:
-        grep_str = [
-            s.strip() for s in f.readlines() if "Total energy is" in s
-        ]  # (e.g. ['Total energy is             -1.117672932144052 a.u.'])
+        # (e.g. ['Total energy is             -1.117672932144052 a.u.'])
+        grep_str = [s.strip() for s in f.readlines() if "Total energy is" in s]
         ref_energy = float(grep_str[-1].split()[-2])  # (e.g. -1.117672932144052)
     # Grep the test output file
     with open(output_file_path, encoding="utf-8", mode="r") as f:
@@ -55,7 +54,8 @@ def test_h2():
 
     # The previous output file is overwritten by the current output file.
     prev_output = "H2.caspt2.out.prev"  # Previous output (After test, the output file is moved to this)
-    prev_file_path = os.path.normpath(os.path.join(test_path, "/", prev_output))
+    prev_file_path = os.path.normpath(os.path.join(test_path, prev_output))
+
     subprocess.run(" ".join(["mv", output_filename, prev_file_path]), shell=True)
     # Check whether the output of test run
     # matches the reference to 7th decimal places.
