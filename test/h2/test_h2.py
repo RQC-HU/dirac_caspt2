34f4dcf90b1ff733c205837355321a06c419fefb
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-06-16 00:02:56 +0900

    Delete shell scripts and calculate using python

--- test/h2/test_h2.py
+++ test/h2/test_h2.py
@@ -1,39 +1,58 @@
 import subprocess
 import os
 import pytest
+import glob
 
 
 def test_h2():
     test_path = os.path.dirname(os.path.abspath(__file__))
     os.chdir(test_path)  # Change directory to dirac_caspt2/test/h2
-    print(test_path)
-    ref_filename = "reference.H2.out"   # Reference
-    output_filename = "H2.caspt2.out"   # Output (This file is compared with Reference)
+    print(test_path)  # Debug output
+    ref_filename = "reference.H2.out"  # Reference
+    output_filename = "H2.caspt2.out"  # Output (This file is compared with Reference)
     prev_output = "H2.caspt2.out.prev"  # Previous output (After test, the output file is moved to this)
+    r4dcasci = os.path.normpath(os.path.join(test_path, "../../bin/r4dcascicoexe"))
+    r4dcaspt2 = os.path.normpath(os.path.join(test_path, "../../bin/r4dcaspt2ocoexe"))
     # Run calculation
-    p = subprocess.run("sh ./h2.sh", shell=True)
-    print("CASCI/CASPT2 status", p.returncode)  # Check status (If p.returncode != 0, calculation failed.)
-    ref = subprocess.run(
-        "cat " + ref_filename + " |  awk ' /Total/{print}' | tr -s '\n'",
+    p = subprocess.run(
+        " ".join(
+            [r4dcasci, "&>", output_filename, "&&", r4dcaspt2, "&>>", output_filename]
+        ),
         shell=True,
-        encoding="utf-8",
-        stdout=subprocess.PIPE,
-    )  # Get CASPT2 energy and e2sum from the output
-    output = subprocess.run(
-        "cat " + output_filename + " | awk '/Total/{print}' | tr -s '\n'",
-        shell=True,
-        encoding="utf-8",
-        stdout=subprocess.PIPE,
-    )  # Get CASPT2 energy and e2sum from the output
-    # Check output for debugging
-    print("Check output for debugging.")
-    print("ref.stdout", ref.stdout)
-    print("ouput.stdout", output.stdout)
-    # Convert string to float
-    ref_energy = float(ref.stdout.split()[-2])
-    output_energy = float(output.stdout.split()[-2])
+    )
+    print(
+        "CASCI/CASPT2 status", p.returncode
+    )  # Check status (If p.returncode != 0, calculation failed.)
+    delete_files = [
+        "[A-H]*int*",
+        "MDCINTNEW",
+        "NEWCICOEFF",
+        "CIMAT*",
+        "e0after",
+        "EPS",
+        "TRANSFOCK"
+    ]
+    # Delete scratch files
+    for d in delete_files:
+        files = glob.glob("".join([test_path, "/", d]))
+        for f in files:
+            os.remove(f)
+
+    # Grep the reference output file
+    with open(os.path.join(test_path, ref_filename), encoding="utf-8", mode="r") as f:
+        grep_str = [
+            s.strip() for s in f.readlines() if "Total energy is" in s
+        ]  # (e.g. ['Total energy is             -1.117672932144052 a.u.'])
+        ref_energy = float(grep_str[-1].split()[-2])  # (e.g. -1.117672932144052)
+    # Grep the test output file
+    with open(
+        os.path.join(test_path, output_filename), encoding="utf-8", mode="r"
+    ) as f:
+        grep_str2 = [s.strip() for s in f.readlines() if "Total energy is" in s]
+        output_energy = float(grep_str2[-1].split()[-2])
+
     # The previous output file is overwritten by the current output file.
-    subprocess.run("mv " + output_filename + " " + prev_output, shell=True)
+    subprocess.run(" ".join(["mv", output_filename, prev_output]), shell=True)
     # Check whether the output of test run
     # matches the reference to 7th decimal places.
     assert output_energy == pytest.approx(ref_energy, 1e-8)
