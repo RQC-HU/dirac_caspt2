27f06742209a10483c19b7d059f7c7933193fb77
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-06-17 15:14:59 +0900

    Create input_reader program

--- src/four_caspt2_module.f90
+++ src/four_caspt2_module.f90
@@ -28,6 +28,9 @@ MODULE four_caspt2_module
     real(8)         :: eshift
     character       :: ptgrp*6
     integer         :: dirac_version
+    integer         :: ras1_start, ras1num, ras2_start, ras2_num, ras3_start, ras3_num
+    integer, allocatable :: ras1_list(:), ras2_list(:), ras3_list(:)
+    integer, parameter :: max_ras1_spinor_num = 200, max_ras2_spinor_num = 200, max_ras3_spinor_num = 200
 
     character       :: date*8, time*10
     integer, allocatable :: idet(:), sp(:), idetr(:)
