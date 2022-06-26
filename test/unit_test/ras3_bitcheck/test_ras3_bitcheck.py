6f151433c9c50ecc646a67e01f8986845c277b7b
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-06-26 13:50:29 +0900

    Refactor test_ras3_bitcheck python script

--- test/unit_test/ras3_bitcheck/test_ras3_bitcheck.py
+++ test/unit_test/ras3_bitcheck/test_ras3_bitcheck.py
@@ -5,25 +5,41 @@ import os
 
 
 def test_ras3_bitcheck():
+    # Current path
     test_path = os.path.dirname(os.path.abspath(__file__))
+
+    # Change directory to the current path
     os.chdir(test_path)
+
+    # input/output/executable file names
     ref_filename = "expected"
-    output_filename = "result"
+    result_filename = "result"
     move_filename = "result.prev"
     exe_filename = "ras3_bitcheck_exe"
-    subprocess.run(os.path.abspath(os.path.join(test_path, exe_filename)), shell=True)
 
-    with open(ref_filename) as file_ref:
+    # Absolute path to input/output/executable files
+    ref_file_path = os.path.abspath(os.path.join(test_path, ref_filename))
+    result_file_path = os.path.abspath(os.path.join(test_path, result_filename))
+    move_file_path = os.path.abspath(os.path.join(test_path, move_filename))
+    exe_file_path = os.path.abspath(os.path.join(test_path, exe_filename))
+
+    # Run tests
+    subprocess.run(exe_file_path, shell=True)
+
+    # Get values from reference
+    with open(ref_file_path) as file_ref:
         string_ref = file_ref.read()
         string_ref = string_ref.split()
-    with open(output_filename) as file_result:
+
+    # Get values from result
+    with open(result_file_path) as file_result:
         string_result = file_result.read()
         string_result = string_result.split()
 
-    shutil.move(
-        os.path.abspath(os.path.join(test_path, output_filename)),
-        os.path.abspath(os.path.join(test_path, move_filename)),
-    )
+    # Move result files to move_file_path
+    shutil.move(result_file_path, move_file_path)
+
+    # Evaluate the difference between references and results
     assert string_ref == string_result
 
 
