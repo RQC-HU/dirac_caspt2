07860f480a7913009f2df298a447d64215658dbc
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-08-13 05:44:52 +0900

    Remove builtin open in test_sort_int_reverse.f90

--- test/unit_test/sort_test/test_sort_int_reverse.f90
+++ test/unit_test/sort_test/test_sort_int_reverse.f90
@@ -5,7 +5,6 @@ program main
     integer :: want_to_sort(21) = (/8, 9, 10, 11, 12, 13, 14, 15, 16, 169, 170, 171, 172, 173, 174, 175, 1, 3, 5, 156, 189/)
     integer :: new_unit = 20
     call heapSort(want_to_sort, .true.)
-    open (1, file='int_reverse.out', form='formatted')
     call open_formatted_file(unit=new_unit, file="int_reverse.out",status='replace' ,optional_action="write")
     write (new_unit, *) want_to_sort
     close (new_unit)
