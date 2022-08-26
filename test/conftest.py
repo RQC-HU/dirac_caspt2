7c01bd42f709f384187de43b5f2702abf880c5a7
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-08-27 01:02:38 +0900

    Add skip control

--- test/conftest.py
+++ test/conftest.py
@@ -1,13 +1,12 @@
 import pytest
 
+slow_opion = "--runslow"
+slowest_opion = "--runslowest"
+
 
 def pytest_addoption(parser):
-    parser.addoption(
-        "--skip-slow",
-        action="store_true",
-        default=False,
-        help="skip slow tests",
-    )
+    parser.addoption(slow_opion, action="store_true", default=False, help="run slow tests")
+    parser.addoption(slowest_opion, action="store_true", default=False, help="run slow tests")
     parser.addoption(
         "--parallel",
         type=int,
@@ -19,3 +18,18 @@ def pytest_addoption(parser):
 @pytest.fixture
 def the_number_of_process(request):
     return request.config.getoption("--parallel")
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "slow: mark test as slow to run")
+    config.addinivalue_line("markers", "slowest: mark test as slowest to run")
+
+
+def pytest_collection_modifyitems(config, items):
+    skip_slow = pytest.mark.skip(reason=f"need {slow_opion} or {slowest_opion} option to run. REASON: Slow test")
+    skip_slowest = pytest.mark.skip(reason=f"need {slowest_opion} option to run. REASON: Slow test")
+    for item in items:
+        if item.get_closest_marker("slowest") and not config.getoption(slowest_opion):
+            item.add_marker(skip_slowest)
+        elif item.get_closest_marker("slow") and not config.getoption(slow_opion):
+            item.add_marker(skip_slow)
