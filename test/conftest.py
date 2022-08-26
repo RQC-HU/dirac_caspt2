72fba055cb1c8ed77d1c6174fbc6dd957e446a89
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-08-27 01:12:19 +0900

    Change pytest mark from slowest to runall

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 
 slow_opion = "--runslow"
-slowest_opion = "--runslowest"
+runall_option = "--runall"
 
 
 def pytest_addoption(parser):
     parser.addoption(slow_opion, action="store_true", default=False, help="run slow tests")
-    parser.addoption(slowest_opion, action="store_true", default=False, help="run slow tests")
+    parser.addoption(runall_option, action="store_true", default=False, help="run all tests")
     parser.addoption(
         "--parallel",
         type=int,
@@ -22,14 +22,14 @@ def the_number_of_process(request):
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
-    config.addinivalue_line("markers", "slowest: mark test as slowest to run")
+    config.addinivalue_line("markers", "runall: mark test as runall to run")
 
 
 def pytest_collection_modifyitems(config, items):
-    skip_slow = pytest.mark.skip(reason=f"need {slow_opion} or {slowest_opion} option to run. REASON: Slow test")
-    skip_slowest = pytest.mark.skip(reason=f"need {slowest_opion} option to run. REASON: Slow test")
+    skip_slow = pytest.mark.skip(reason=f"need {slow_opion} or {runall_option} option to run. REASON: Slow test")
+    skip_slowest = pytest.mark.skip(reason=f"need {runall_option} option to run. REASON: Slow test")
     for item in items:
-        if item.get_closest_marker("slowest") and not config.getoption(slowest_opion):
+        if item.get_closest_marker("runall") and not config.getoption(runall_option):
             item.add_marker(skip_slowest)
         elif item.get_closest_marker("slow") and not config.getoption(slow_opion):
             item.add_marker(skip_slow)
