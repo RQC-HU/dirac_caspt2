e3b35d0d0b0bd15012d438d823d20de16472922b
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-09-13 22:18:55 +0900

    onlyslow option changed to slowonly. slow marker also changed to slowonly.

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 
-slow_opion = "--onlyslow"
+slow_only_option = "--slowonly"
 dev_option = "--dev"
 runall_option = "--all"
 
 
 def pytest_addoption(parser):
-    parser.addoption(slow_opion, action="store_true", default=False, help="run only very slow tests")
+    parser.addoption(slow_only_option, action="store_true", default=False, help="run only very slow tests")
     parser.addoption(dev_option, action="store_true", default=False, help="run tests for development")
     parser.addoption(runall_option, action="store_true", default=False, help="run all tests")
     parser.addoption(
@@ -23,12 +23,12 @@ def the_number_of_process(request):
 
 
 def pytest_configure(config):
-    config.addinivalue_line("markers", "slow: mark test as slow to run")
+    config.addinivalue_line("markers", "slowonly: mark test as slow to run")
     config.addinivalue_line("markers", "dev: mark test as for development")
 
 
 def pytest_collection_modifyitems(config, items):
-    skip_slow = pytest.mark.skip(reason=f"need {runall_option} or {slow_opion} option to run. REASON: slow test")
+    skip_slow = pytest.mark.skip(reason=f"need {runall_option} or {slow_only_option} option to run. REASON: slow test")
     skip_tests_because_dev = pytest.mark.skip(reason=f"need no option or {runall_option} option to run. REASON: --dev was activated")
     skip_fast_dev = pytest.mark.skip(reason=f"need no option or {dev_option} or {runall_option} option or  to run. REASON: --onlyslow was activated")
     skip_fast_neutral = pytest.mark.skip(reason=f"need no option or {runall_option} option or  to run. REASON: --onlyslow was activated")
@@ -38,12 +38,12 @@ def pytest_collection_modifyitems(config, items):
     for item in items:
         if item.get_closest_marker("dev"):
             # dev tests always run except when --onlyslow was activated
-            if config.getoption(slow_opion):
+            if config.getoption(slow_only_option):
                 item.add_marker(skip_fast_dev)
             else:
                 pass
-        elif item.get_closest_marker("slow"):
-            if config.getoption(slow_opion):
+        elif item.get_closest_marker("slowonly"):
+            if config.getoption(slow_only_option):
                 pass
             else:  # no args related to markers
                 item.add_marker(skip_slow)
@@ -51,5 +51,5 @@ def pytest_collection_modifyitems(config, items):
             # Skip neutral tests if --dev or --onlyslow were activated
             if config.getoption(dev_option):
                 item.add_marker(skip_tests_because_dev)
-            elif config.getoption(slow_opion):
+            elif config.getoption(slow_only_option):
                 item.add_marker(skip_fast_neutral)
