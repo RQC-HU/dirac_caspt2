a783470028d0fd711b0a534567824c1fd562c9b7
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-10-05 17:15:41 +0900

    Refactor timing.f90

--- src/timing.f90
+++ src/timing.f90
@@ -12,10 +12,7 @@ SUBROUTINE timing(date0, tsec0, date, tsec)
     integer              ::  val(8), day, hour, min
 
     Call DATE_AND_TIME(VALUES=val)
-!        Write(*,*)'Year = ',val(1),'Mon = ',val(2),'Date = ',val(3)
-!        Write(*,*)'Hour = ',val(5),'Min = ',val(6),'Sec = ',val(7),'.',val(8)
 
-    ! val(8): millisec, val(7): sec, val(6):min, val(5):hours
     tsec = val(8)*(1.0d-03) + val(7) + val(6)*(6.0d+01) + val(5)*(6.0d+01)**2
 
     if (date0 < val(3)) then
@@ -26,8 +23,8 @@ SUBROUTINE timing(date0, tsec0, date, tsec)
 
     if (rank == 0) then
         print '("Present time is")'
-        print '("year  = ",I4,"month = ",I4,"date  = ",I4 )', val(1), val(2), val(3)
-        print '(14X,I4,"h   ",I4,"min  ",I2,".",I3,"sec  " )',&
+        print '("year  = ",I4, "month = ",I4, "date  = ",I4 )', val(1), val(2), val(3)
+        print '(14X, I4, "h   ",I4, "min  ",I2, ".",I3, "sec  " )',&
         & val(5), val(6), val(7), val(8)
     end if
 
@@ -42,7 +39,7 @@ SUBROUTINE timing(date0, tsec0, date, tsec)
 
     sec = resd
     if (rank == 0) then
-        write (*, '("computational time = ",I3,"day",I3,"h ",I3, &
-        &"min",F7.3,"sec")') day, hour, min, sec
+        print '("computational time = ",I3, "day",I3, "h ",I3, &
+        &"min",F7.3, "sec")', day, hour, min, sec
     end if
 end subroutine timing
