9a629ef3b49609be505fdeab2bba5c164dc65331
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2022-12-01 17:53:31 +0900

    Fix preprocessor

--- src/module_error.f90
+++ src/module_error.f90
@@ -1,15 +1,18 @@
 module module_error
+    use four_caspt2_module, only: ierr
     implicit none
+#ifdef HAVE_MPI
     include 'mpif.h'
+#endif
     private
     public :: stop_with_errorcode
 contains
     subroutine stop_with_errorcode(errorcode)
         implicit none
         integer, intent(in) :: errorcode
-# ifdef  MPI
-        call MPI_ABORT(MPI_COMM_WORLD, errorcode, MPI_IERROR)
-# endif
+#ifdef HAVE_MPI
+        call MPI_ABORT(MPI_COMM_WORLD, errorcode, ierr)
+#endif
         call exit(errorcode)
     end subroutine stop_with_errorcode
 end module module_error
