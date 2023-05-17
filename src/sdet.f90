05b3a82669f88081c537fc8084adccf1be52c0e4
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2023-05-18 00:22:42 +0900

    Add description about using Lapack subroutines part.
    Use dgesv instead of  ZGETRF and ZGETRI in traci subroutine.

--- src/sdet.f90
+++ src/sdet.f90
@@ -12,11 +12,11 @@ SUBROUTINE dets(fa, occold, occnew, ds)
     Implicit NONE
     real(8), intent(in)  :: fa(ninact + 1:ninact + nact, ninact + 1:ninact + nact)
     integer, intent(in)  :: occold(nelec), occnew(nelec)
-    complex*16, intent(out) :: ds
+    real(8), intent(out) :: ds
 
-    integer                  :: i0, j0, i, info, ipvt(nelec), n
-    complex*16               :: sini(nelec, nelec), work(nelec), det(2), ds2
-    real(8)                   :: phase
+    integer :: i0, j0, i, info, ipvt(nelec), n
+    real(8) :: sini(nelec, nelec), work(nelec), det(2)
+    real(8) :: phase
 
 ! +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 ! +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
@@ -26,7 +26,6 @@ SUBROUTINE dets(fa, occold, occnew, ds)
     sini = 0.0d+00
     work = 0.0d+00
     det = 0.0d+00
-    ds = 0.0d+00
 
     Do i0 = 1, nelec
         Do j0 = 1, nelec
@@ -34,22 +33,22 @@ SUBROUTINE dets(fa, occold, occnew, ds)
         End do
     End do
 
-    Call zgetrf(nelec, nelec, sini, nelec, ipvt, info)   ! From lapack LU fatorization!
+    ! LU factorization by lapack routine dgetrf
+    ! dgetrf: https://netlib.org/lapack/explore-html/dd/d9a/group__double_g_ecomputational_ga0019443faea08275ca60a734d0593e60.html
+    ! sini is the matrix to be factorized
+    ! sini is overwritten by the LU factorization of sini
+    ! ipvt is the pivot indices of the LU factorization.
+    Call dgetrf(nelec, nelec, sini, nelec, ipvt, info)   ! From lapack LU fatorization!
 
     ! count the number of ipvt(i) /= i
     n = count(ipvt /= [(i, i=1, nelec)])
-
     phase = sign_even_ret1(n) ! If n is even, phase = 1.0d+00, else phase = -1.0d+00
 
-    ds2 = 1.0d+00
-
+    ds = 1.0d+00
     Do i = 1, nelec
-        ds2 = sini(i, i)*ds2
+        ds = sini(i, i)*ds
     End do
-
-    ds2 = ds2*phase
-
-    ds = ds2
+    ds = ds*phase
 
 End subroutine dets
 
