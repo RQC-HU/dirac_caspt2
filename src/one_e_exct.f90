fad3ab6b361532ba29a612ff21b4a9322c174f6d
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2023-05-18 10:03:06 +0900

    Change name of variables
    indv, indu are no longer needed.

--- src/one_e_exct.f90
+++ src/one_e_exct.f90
@@ -1,6 +1,6 @@
 ! ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-subroutine one_e_exct(icas_idx, creat, anhi, newcas_idx, phase)
+subroutine one_e_exct(icas_idx, creation_op, annihilation_op, newcas_idx, phase)
 
 ! This subroutine is used to calculate the phase and
 ! the new CAS index for the one-electron excitation
@@ -11,10 +11,12 @@ subroutine one_e_exct(icas_idx, creat, anhi, newcas_idx, phase)
 
     Implicit NONE
 
-    integer, intent(in)  :: icas_idx, creat, anhi
+    ! icas_idx: index of the current CAS
+    ! creation_op: index of creation operator (electron will be excited to this orbital)
+    ! annihilation_op: index of annihilation operator (electron will be excited from this orbital)
+    integer, intent(in)  :: icas_idx, creation_op, annihilation_op
     integer, intent(out) :: newcas_idx, phase
-
-    integer :: ia, ib, indu, indv
+    integer :: ia, ib
 
     ia = 0
     ib = 0
@@ -23,26 +25,23 @@ subroutine one_e_exct(icas_idx, creat, anhi, newcas_idx, phase)
 
 !     Index for only CASCI Active space
 
-    indu = creat
-    indv = anhi
-
-    if ((indv == indu) .and. (btest(icas_idx, indv - 1) .eqv. .true.)) then
+    if ((annihilation_op == creation_op) .and. (btest(icas_idx, annihilation_op - 1) .eqv. .true.)) then
 
         newcas_idx = icas_idx
         phase = 0
 
-    elseif ((btest(icas_idx, indv - 1) .eqv. .true.) .and. (btest(icas_idx, indu - 1) .eqv. .false.)) then
+    elseif ((btest(icas_idx, annihilation_op - 1) .eqv. .true.) .and. (btest(icas_idx, creation_op - 1) .eqv. .false.)) then
 
-        newcas_idx = icas_idx - 2**(indv - 1) + 2**(indu - 1)
+        newcas_idx = icas_idx - 2**(annihilation_op - 1) + 2**(creation_op - 1)
+
 !        calculation of phase
-
-        if (indv < nact) then
-            ia = ia + 2**nact - 2**indv
+        if (annihilation_op < nact) then
+            ia = ia + 2**nact - 2**annihilation_op
         end if
-        if (indu < nact) then
-            ib = ib + 2**nact - 2**indu
+        if (creation_op < nact) then
+            ib = ib + 2**nact - 2**creation_op
         end if
-        phase = POPCNT(iand(icas_idx, ia)) + POPCNT(iand(icas_idx - 2**(indv - 1), ib))
+        phase = POPCNT(iand(icas_idx, ia)) + POPCNT(iand(icas_idx - 2**(annihilation_op - 1), ib))
         ! odd => (-), even => (+)
 
     end if
