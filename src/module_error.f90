7334fae6bf7ffcec5367596f4274da66c2f5a2f1
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2023-07-19 22:08:06 +0900

    fix: Delete MPI_Barrier in the stop_with_errorcode subroutine.
    Because if a process other than the master fails to open a file, this program hangs.

--- src/module_error.f90
+++ src/module_error.f90
@@ -1,20 +1,12 @@
 module module_error
     ! This module contains a subroutine to stop the program with a given error code.
     implicit none
-#ifdef HAVE_MPI
-    include 'mpif.h'
-#endif
     private
     public :: stop_with_errorcode
 contains
     subroutine stop_with_errorcode(errorcode)
         implicit none
         integer, intent(in) :: errorcode
-        integer :: ierr
-        ierr = 0
-#ifdef HAVE_MPI
-        call MPI_Barrier(MPI_COMM_WORLD, ierr)
-#endif
         print *, "Exit with error", errorcode
 #ifdef INTEL
         call TRACEBACKQQ("Exit with error", errorcode)
