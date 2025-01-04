46a06670ff99313b8cc6dd07c0e1f30d6c01ef36
Kohei Noda <ponserviceregist@gmail.com>
2025-01-04 15:59:33 +0900

    gen_dcaspt2_restart test: add multi cirtoos test

--- test/unit_test/gen_dcapst2_restart/test_gen_dcaspt2_restart.py
+++ test/unit_test/gen_dcapst2_restart/test_gen_dcaspt2_restart.py
@@ -50,6 +50,26 @@ def test_pass_in_the_middle_subspace_broken(env_setup_gen_restart_file) -> None:
 
 
 @pytest.mark.dev
+def test_pass_multi_ciroots(env_setup_gen_restart_file) -> None:
+    (gen_restart_path, test_path, input_path, expected_path) = env_setup_gen_restart_file
+    os.chdir(test_path)
+    print(f"input_path: {input_path}")
+    command = f"{gen_restart_path} {input_path}"
+    subprocess.run(command.split(), check=True)
+
+    for trailing_file_str in ["_2_1", "_2_2"]:
+        string_ref = get_split_string_list_from_output_file(f"{expected_path}{trailing_file_str}")
+        string_result = get_split_string_list_from_output_file(f"caspt2_restart{trailing_file_str}")
+
+        for ref, result in zip(string_ref, string_result):
+            assert ref == result
+    # string_ref = get_split_string_list_from_output_file(expected_path)
+    # string_result = get_split_string_list_from_output_file("caspt2_restart_4_1")
+
+    # for ref, result in zip(string_ref, string_result):
+    #     assert ref == result
+
+@pytest.mark.dev
 def test_no_output(env_setup_gen_restart_file) -> None:
     (gen_restart_path, test_path, input_path, expected_path) = env_setup_gen_restart_file
     os.chdir(test_path)
