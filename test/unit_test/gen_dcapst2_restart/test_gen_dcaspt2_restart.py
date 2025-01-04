33cba765de7fb32ca2e297957f02a04b0846dc0d
Kohei Noda <ponserviceregist@gmail.com>
2025-01-04 15:01:46 +0900

    test: support new restart file

--- test/unit_test/gen_dcapst2_restart/test_gen_dcaspt2_restart.py
+++ test/unit_test/gen_dcapst2_restart/test_gen_dcaspt2_restart.py
@@ -15,7 +15,7 @@ def test_pass_subspace_a_and_b(env_setup_gen_restart_file) -> None:
     subprocess.run(command.split(), check=True)
 
     string_ref = get_split_string_list_from_output_file(expected_path)
-    string_result = get_split_string_list_from_output_file("caspt2_restart")
+    string_result = get_split_string_list_from_output_file("caspt2_restart_2_1")
 
     for ref, result in zip(string_ref, string_result):
         assert ref == result
@@ -29,7 +29,7 @@ def test_pass_full_subspaces(env_setup_gen_restart_file) -> None:
     subprocess.run(command.split(), check=True)
 
     string_ref = get_split_string_list_from_output_file(expected_path)
-    string_result = get_split_string_list_from_output_file("caspt2_restart")
+    string_result = get_split_string_list_from_output_file("caspt2_restart_33_1")
 
     for ref, result in zip(string_ref, string_result):
         assert ref == result
@@ -43,19 +43,19 @@ def test_pass_in_the_middle_subspace_broken(env_setup_gen_restart_file) -> None:
     subprocess.run(command.split(), check=True)
 
     string_ref = get_split_string_list_from_output_file(expected_path)
-    string_result = get_split_string_list_from_output_file("caspt2_restart")
+    string_result = get_split_string_list_from_output_file("caspt2_restart_3_1")
 
     for ref, result in zip(string_ref, string_result):
         assert ref == result
 
 
 @pytest.mark.dev
-def test_fail(env_setup_gen_restart_file) -> None:
+def test_no_output(env_setup_gen_restart_file) -> None:
     (gen_restart_path, test_path, input_path, expected_path) = env_setup_gen_restart_file
     os.chdir(test_path)
     command = f"{gen_restart_path} {input_path}"
     p = subprocess.run(command.split(), capture_output=True)
-    assert p.returncode != 0 and "ValueError" in p.stderr.decode("utf-8")
+    assert p.returncode == 0 and p.stdout.decode("utf-8") == "" and p.stderr.decode("utf-8") == ""
 
 
 @pytest.mark.dev
