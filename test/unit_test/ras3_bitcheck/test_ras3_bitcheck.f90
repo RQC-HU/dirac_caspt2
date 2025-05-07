ccefd0834a8266283a701fab44113d6702760a40
Kohei Noda <ponserviceregist@gmail.com>
2025-05-08 00:16:42 +0900

    test: unit_test: ras3_bitcheck: avoid type mismatch

--- test/unit_test/ras3_bitcheck/test_ras3_bitcheck.f90
+++ test/unit_test/ras3_bitcheck/test_ras3_bitcheck.f90
@@ -1,4 +1,5 @@
 program ras3_bitcheck
+    use, intrinsic :: iso_fortran_env, only: int64
     use module_global_variables
     use module_file_manager
     use read_input_module
@@ -13,7 +14,7 @@ program ras3_bitcheck
 
     call open_formatted_file(unit=unit_new, file='result.out', status="replace", optional_action='write')
     do i = 1, 2**nact - 1
-        is_allow = satisfy_ras3_condition(i, ras3_max_elec)
+        is_allow = satisfy_ras3_condition(int(i, kind=int64), int(ras3_max_elec, kind=int64))
         if (is_allow) then
             print '(i4,b20)', i, i
             write (unit_new, '(i4,b20)') i, i
