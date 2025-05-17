fffa5ee5f44542567494a42fb2411b99b2e1b991
Kohei Noda <ponserviceregist@gmail.com>
2025-05-17 16:41:10 +0000

    read_input_module.F90: check whether comment line is when read subprograms section

--- src/read_input_module.F90
+++ src/read_input_module.F90
@@ -596,6 +596,7 @@ contains
         integer :: iostat
         character(len=max_str_length) :: input
         character(:), allocatable :: trim_input
+        logical :: is_comment
 
         do while (.true.)
             read (unit_num, '(A)', iostat=iostat) input
@@ -606,12 +607,14 @@ contains
             end if
 
             call uppercase(input)
+            call is_comment_line(input, is_comment)
+            if (is_comment) cycle
             allocate (trim_input, source=trim(adjustl(input)))
             if (index(trim_input, ".") == 1) then
                 ! If the input starts with a dot, it is the end of the subprograms.
                 ! Need to reset the file pointer to the beginning of the line.
                 backspace (unit_num)
-                return
+                exit
             end if
 
             select case (trim_input)
