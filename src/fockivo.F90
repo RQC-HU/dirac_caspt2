3b0de09d1cdbde0319470384a7217d351a883e9d
Kohei Noda <ponserviceregist@gmail.com>
2026-03-04 22:00:50 +0900

    fockivo.F90: support IVO for NZ=2
    but currently don't support IVO calculation for NZ=4(quaternion)

--- src/fockivo.F90
+++ src/fockivo.F90
@@ -16,7 +16,7 @@ SUBROUTINE fockivo ! TO MAKE FOCK MATRIX for IVO
     Implicit NONE
 
     integer      :: j, i, k, i0, j0
-    integer      :: isym, nv, numh
+    integer      :: isym, nv, numh, buf_idx
     integer      :: imo, iao, unit_buf
     real(8)      :: thresd
     complex*16, allocatable  :: fsym(:, :) ! Symmetrized fock_ivo_matrix for particular irrep
@@ -209,9 +209,22 @@ SUBROUTINE fockivo ! TO MAKE FOCK MATRIX for IVO
 
         do iao = 1, num_ao
             do imo = 1, num_mo
-                BUF(offset_buf + (imo - 1)*num_ao + iao) = DBLE(itrfmo(iao, imo))
+                buf_idx = offset_buf + (imo - 1)*num_ao + iao
+                BUF(buf_idx) = DBLE(itrfmo(iao, imo))
             end do
         end do
+
+        if (cmo_nz == 2) then
+            do iao = 1, num_ao
+                do imo = 1, num_mo
+                    buf_idx = offset_buf + (imo - 1)*num_ao + iao
+                    ! size(BUF)/cmo_nz + buf_idx is a imaginary part idx of the CMO
+                    BUF(size(BUF)/cmo_nz + buf_idx) = DIMAG(itrfmo(iao, imo))
+                end do
+            end do
+        end if
+
+        ! TODO: impl quaternion (NZ = 4)
         deallocate (itrfmo)
     end do
 
@@ -225,7 +238,13 @@ contains
 
         do iao = 1, num_ao
             do imo = 1, num_mo
-                itrfmo(iao, imo) = BUF(offset_buf + (imo - 1)*num_ao + iao)
+                buf_idx = offset_buf + (imo - 1)*num_ao + iao
+                if (cmo_nz == 1) then
+                    itrfmo(iao, imo) = BUF(buf_idx)
+                else if (cmo_nz == 2) then
+                    itrfmo(iao, imo) = DCMPLX(BUF(buf_idx), BUF(size(BUF)/cmo_nz + buf_idx))
+                end if
+                ! TODO: impl quaternion (NZ = 4)
             end do
         end do
     end subroutine create_itrfmo
